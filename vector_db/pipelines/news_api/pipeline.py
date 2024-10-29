31f895b307141230cca4d0d934a58bfe9dbbdf81
Jakub <jakubmalerek@gmail.com>
2024-10-29 18:40:13 +0100

    refactor ETL pipeline for news api

--- vector_db/pipelines/news_api/pipeline.py
+++ vector_db/pipelines/news_api/pipeline.py
@@ -1,9 +1,9 @@
 from pipelines.news_api.extract import get_ai_news
 from pipelines.news_api.transform import transform_data
-from pipelines.news_api.load import load_document
+from pipelines.news_api.load import bulk_load_documents
 
 
-def run_etl(news_endpoint: str, news_api_key: str, es_creds: dict, index_name: str):
+def run_etl(news_endpoint: str, news_api_key: str, es_instance: str, index_name: str):
     news_data = get_ai_news(endpoint=news_endpoint, api_key=news_api_key)
     transformed_data = [transform_data(news) for news in news_data]
-    load_document(es_creds=es_creds, index_name=index_name, data=transformed_data)
+    bulk_load_documents(es=es_instance, index_name=index_name, documents=transformed_data)
